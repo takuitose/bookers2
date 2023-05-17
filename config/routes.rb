d0e9031b6396220f695ebcbccd5187fd49436ecd
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-17 02:13:36 +0000

    [update]bookers2(05171113)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
+  devise_for :users
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
