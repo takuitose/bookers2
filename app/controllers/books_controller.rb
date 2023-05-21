03e6d53fb9db98739c2cee1db56fb1c041a4641e
takuitose <132122425+takuitose@users.noreply.github.com>
2023-05-21 08:44:44 +0000

    [update]bookers2(05211744)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
 
     if @book.save
+      flash[:notice] = "You have created book seccessfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
